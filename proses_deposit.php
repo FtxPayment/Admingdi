8b3f6e03735d1caecc6b10cbc3913cf1331ce9a7
FtxPayment <ibnujuara123@gmail.com>
2025-05-04 01:23:47 +0700

    Delete proses_deposit.php

--- proses_deposit.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-$nama = $_POST['nama'];
-$jumlah = $_POST['jumlah'];
-
-$koneksi->query("INSERT INTO deposit (nama, jumlah) VALUES ('$nama', '$jumlah')");
-header("Location: index.php");
-?>
